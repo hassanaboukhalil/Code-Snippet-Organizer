7f43553b5fe5b97c5ab9b6982ac46f9ef2bd4a71
hassan <hassanak509@gmail.com>
2025-03-21 13:41:30 +0200

    updated AuthController.php file

--- server/app/Http/Controllers/v1/AuthController.php
+++ server/app/Http/Controllers/v1/AuthController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\v1;
 
 use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -20,19 +19,4 @@ class AuthController extends Controller
             "success" => true
         ]);
     }
-
-    // function login(Request $request)
-    // {
-    //     $credentials = [
-    //         "email" => $request['email'],
-    //         'password' => $request['password']
-    //     ];
-
-    //     $user = Auth::user();
-
-    //     return response()->json([
-    //         'success' => true,
-    //         'user' => $user
-    //     ]);
-    // }
 }
