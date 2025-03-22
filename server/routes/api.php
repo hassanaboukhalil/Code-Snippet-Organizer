d388d969c944a15acabcd1189ae0b7497b087c3a
hassan <hassanak509@gmail.com>
2025-03-22 17:32:49 +0200

    updated the middleware auth:api

--- server/routes/api.php
+++ server/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\v1\AuthController;
+use App\Http\Controllers\v1\User\SnippetController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,17 +10,18 @@ use Illuminate\Support\Facades\Route;
 // })->middleware('auth:sanctum');
 
 
-
-//Unauthenticated Routes
-// Route::group(['prefix' => 'guest'], function(){
-//     Route::post('/login', [AuthController::class, 'login']);
-//     Route::post('/signup', [AuthController::class, 'signup'])
-// })
-
-
-
-//Unauthenticated Routes
 Route::group(['prefix' => 'v1'], function () {
+    Route::group(["middleware" => "auth:api"], function () {
+        Route::group(['prefix' => 'user'], function () {
+            Route::get('/getSnippets', [SnippetController::class, 'getSnippets']);
+            Route::post('/addSnippet', [SnippetController::class, 'addSnippet']);
+            Route::post('/updateSnippet', [SnippetController::class, 'updateSnippet']);
+            Route::post('/deleteSnippet', [SnippetController::class, 'deleteSnippet']);
+        });
+    });
+
+
+    //Unauthenticated Routes
     Route::group(['prefix' => 'guest'], function () {
         Route::post('/login', [AuthController::class, 'login']);
         Route::post('/signup', [AuthController::class, 'signup']);
